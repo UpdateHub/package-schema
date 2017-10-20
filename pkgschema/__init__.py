4ad37d06441ff14c02f4333b51cb6afdffa31555
Pablo Palácios <ppalacios992@gmail.com>
2017-10-20 14:17:19 -0200

    validators: creates validate_objects_filenames validator
    This is a workaround to validate what is not possible to do with
    pure JSON Schema. This validator prevents users to create a metadata
    with objects sets with different files.
    
    To be clear, by now, we only allow users to have different
    configurations between sets, but they must have the same filenames.
    
    Signed-off-by: Pablo Palácios <ppalacios992@gmail.com>

--- pkgschema/__init__.py
+++ pkgschema/__init__.py
@@ -33,9 +33,18 @@ def validate_schema(schema_fn, obj):
         raise ValidationError(err)
 
 
+def validate_objects_filenames(objects):
+    sets = [[obj['filename'] for obj in sets] for sets in objects]
+    for set_ in sets:
+        if set_ != sets[0]:
+            error = 'Objects filenames are different between sets'
+            raise ValidationError(error)
+
+
 def validate_metadata(metadata):
     """Validates a package metadata against package schema.
 
     Returns None if valid, else raises ValidationError.
     """
-    return validate_schema('metadata.json', metadata)
+    validate_schema('metadata.json', metadata)
+    validate_objects_filenames(metadata['objects'])
